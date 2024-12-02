4b8aca0994208c58fa190175810af6ab90f6d95f
chae <yce0222@gmail.com>
2024-12-02 19:13:16 +0900

    feat: Implement account professional info - #14

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { AdminList } from './pages/admin-list';
 import { ResourceManage } from './pages/resource';
 import { KnowledgeManage } from './pages/knowledge-manage';
 import { NewsManage } from './pages/news-manage';
+import { Account } from './pages/account';
 
 export default function App() {
   return (
@@ -30,6 +31,7 @@ export default function App() {
                 <Route path="/resource" element={<ResourceManage />} />
                 <Route path="/knowledge" element={<KnowledgeManage />} />
                 <Route path="/news" element={<NewsManage />} />
+                <Route path="/account" element={<Account />} />
               </Route>
               <Route path="*" element={<NoMatch />} />
             </Routes>
---
+++ src/pages/account/index.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from 'react';
+import { Page } from '../../components/page';
+import { ProfessionalInfo } from './components/professional-info';
+import './style.css';
+
+export const Account: FunctionComponent<{}> = () => {
+  return (
+    <Page title="Account">
+      <h1 className='title'>Account setting</h1>
+
+      {/* My Profile */}
+      
+      {/* Professional Info */}
+      <ProfessionalInfo />
+    </Page>
+  );
+};
