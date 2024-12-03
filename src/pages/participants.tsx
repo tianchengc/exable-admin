4ff0841b9766c8d2652355af5c59ca79afbf7870
Javed <shahjavedgm@gmail.com>
2024-12-03 00:30:46 -0500

    implement basic table with mock data

--- src/pages/participants.tsx
+++ src/pages/participants.tsx
@@ -4,11 +4,101 @@
 // import Ant table
 // async/await error handling in use effect
 // import { Link } from 'react-router-doc
+import { Table, DatePicker } from 'antd';
 
 function Participants() {
-  // const [patientData, setPatientData] = useState([]);
+  const mockData = [
+    {
+      id: 1,
+      name: 'batman',
+      age: 23,
+      chronic_condition: 'chronic diesease',
+      caat: 0.7,
+      mmrc: 1,
+      memberSince: '01-02-2017',
+    },
+    {
+      id: 2,
+      name: 'Fuzzy Wuzzy',
+      age: 39,
+      chronic_condition: 'chronic diesease',
+      caat: 0.7,
+      mmrc: 1,
+      memberSince: '01-02-2017',
+    },
+    {
+      id: 3,
+      name: 'John Doe',
+      age: 53,
+      chronic_condition: 'chronic diesease',
+      caat: 0.7,
+      mmrc: 1,
+      memberSince: '01-02-2017',
+    },
+  ];
 
-  /* Use this with API
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text: string, record: { id: string }) => {
+        return (
+          <a
+            onClick={() => {
+              console.log(`/participants/${record.id}`);
+            }}
+          >
+            {text}
+          </a>
+        );
+      },
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Chronic Condition',
+      dataIndex: 'chronic_condition',
+      key: 'chronic_condition',
+    },
+    {
+      title: 'CAAT',
+      dataIndex: 'caat',
+      key: 'caat',
+    },
+    {
+      title: 'mMRC',
+      dataIndex: 'mmrc',
+      key: 'mmrc',
+    },
+    {
+      title: 'Member Since',
+      dataIndex: 'memberSince',
+      key: 'memberSince',
+    },
+  ];
+
+  return (
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+      <div className="flex gap-20">
+        <h1>Hello Style!</h1>
+        <DatePicker.RangePicker />
+      </div>
+      <Table dataSource={mockData} columns={columns} pagination={true} />;
+    </div>
+  );
+}
+
+export default Participants;
+
+// const [patientData, setPatientData] = useState([]);
+
+/* Use this with API
   useEffect(()=>{
 		const fetchPatientData = async ()=>{
 			try{
@@ -22,13 +112,3 @@ function Participants() {
 		fetchPatientData()
 	},[])
   */
-
-  return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-      <h1>Hello Style!</h1>
-      <p>add a little style here</p>
-    </div>
-  );
-}
-
-export default Participants;
