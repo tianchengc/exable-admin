1d92a34eb39d99c5bc06430872d5259737077659
Swapnil <swapnil118@icloud.com>
2024-12-05 22:24:08 -0500

    Text color for button under program information modified

--- src/components/program-information.tsx
+++ src/components/program-information.tsx
@@ -5,13 +5,13 @@ const ProgramInformation = () => {
     <div className="rounded-md w-[520px] px-4 bg-[#49A0BB] flex flex-col ">
       <p className="text-white text-lg mt-2">Program Information</p>
       <div className="flex p-4 pb-2 flex-wrap gap-4  ">
-        <Button className="rounded-full bg-background-color border-none">
+        <Button className="rounded-full bg-background-color border-none text-secondary-color">
           "Tell us more" data
         </Button>
-        <Button className="rounded-full ml-auto bg-background-color border-none">
+        <Button className="rounded-full ml-auto bg-background-color border-none text-secondary-color">
           "FFB program enrolment" data
         </Button>
-        <Button className=" rounded-full bg-background-color border-none">
+        <Button className=" rounded-full bg-background-color border-none text-secondary-color">
           "Post-Cycle Questionnaire" data
         </Button>
 
