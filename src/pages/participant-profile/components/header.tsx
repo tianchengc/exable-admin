5b618de53b0bd33ea8cfde492a3a6e00216d2a69
Swapnil <swapnil118@icloud.com>
2024-12-16 22:08:12 -0500

    More styling to header

--- src/pages/participant-profile/components/header.tsx
+++ src/pages/participant-profile/components/header.tsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router';
 
 const Header = () => {
   return (
-    <header className=" flex flex-col w-[1090px] ">
+    <header className=" flex flex-col  w-[1090px] ">
       <p className="text-secondary-color mb-5 ">
         <Link to={'/participants'}>
           <ArrowLeftOutlined className="mr-1" /> Back to participant list
         </Link>
       </p>
-      <section className="flex justify-between border border-[silver] bg-[white] rounded-md p-6 px-10">
-        <p>Patricia Bucket</p>
-        <p>Age</p>
-        <p>Gender</p>
-        <p>Chronic condition</p>
-        <p>[CAAT]</p>
-        <p>[mMRC]</p>
-        <p>Participant since</p>
+      <section className="flex justify-between border border-[silver] bg-header rounded-md p-6 px-10">
+        <p className="font-bold">Patricia Bucket</p>
+        <p className="text-sm">Age</p>
+        <p className="text-sm">Gender</p>
+        <p className="text-sm">Chronic condition</p>
+        <p className="text-sm">[CAAT]</p>
+        <p className="text-sm">[mMRC]</p>
+        <p className="text-sm">Participant since</p>
         <Button className="bg-background-color">Message</Button>
       </section>
     </header>
